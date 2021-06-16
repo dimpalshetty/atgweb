cd88f466bd007d33c12241ce823e2c5d6640436a
dimpalshetty <dimpalshetty2000@gmail.com>
2021-06-16 13:33:22 +0530

    body of webpage

--- src/components/body.js
+++ src/components/body.js
@@ -4,39 +4,171 @@ import whole from '../images/whole.png';
 import '../components/body.css';
 import SearchIcon from "@material-ui/icons/Search";
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import GroupAddIcon from '@material-ui/icons/GroupAdd';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function Body() {
+
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
   return (
 
 
-<div className="PostLink">
- <nav class="navbar navbar-expand-lg pl-md-5 ">
-    <div class="container" id="link">
-      <ul class="navbar-nav">
-        <li class="nav-item ">
-          <a class="nav-link" href="#">ALL POSTS(32)</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="##">ARTICLE</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="##">EVENT</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link "  href="##">EDUCATION</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " href="##">JOB</a>
-        </li>
-      </ul>
+    <div className="PostLink">
+
+      <div className="bloc-tabs">
+        <a
+          className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(1)}
+        >
+          All Posts(32)
+        </a>
+        <a
+          className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(2)}
+        >
+          Tab 2
+        </a>
+        <a
+          className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(3)}
+        >
+          Tab 3
+        </a>
+        <a
+          className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(4)}
+        >
+          Tab 4
+        </a>
+        <a
+          className={toggleState === 5 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(5)}
+        >
+          Tab 5
+        </a>
+
+        <div class="btn-group">
+  <button type="button" class="btn btn-light dropdown-toggle btn-sm" data-bs-toggle="dropdown static" aria-expanded="false">
+    Action
+  </button>
+  <ul class="dropdown-menu ">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <li><hr class="dropdown-divider"/></li>
+    <li><a class="dropdown-item" href="#">Separated link</a></li>
+  </ul>
+</div>
+<div className="singlebutton"><button type="button"  class="btn btn-primary btn-sm"> <GroupAddIcon/> Primary</button> </div>
+         
+      </div>
+      <div className="content-tabs">
+        <div
+          className={toggleState === 1 ? "content  active-content" : "content"}
+        >
+          <h2>Content 1</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            praesentium incidunt quia aspernatur quasi quidem facilis quo nihil
+            vel voluptatum?
+          </p>
+        </div>
+
+        <div
+          className={toggleState === 2 ? "content  active-content" : "content"}
+        >
+          <h2>Content 2</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            voluptatum qui adipisci.
+          </p>
+        </div>
+
+        <div
+          className={toggleState === 3 ? "content  active-content" : "content"}
+        >
+          <h2>Content 3</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
+            nostrum rerum laudantium totam unde adipisci incidunt modi alias!
+            Accusamus in quia odit aspernatur provident et ad vel distinctio
+            recusandae totam quidem repudiandae omnis veritatis nostrum
+            laboriosam architecto optio rem, dignissimos voluptatum beatae
+            aperiam voluptatem atque. Beatae rerum dolores sunt.
+          </p>
+        </div>
+        <div
+          className={toggleState === 4 ? "content  active-content" : "content"}
+        >
+          <h2>Content 4</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
+            nostrum rerum laudantium totam unde adipisci incidunt modi alias!
+            Accusamus in quia odit aspernatur provident et ad vel distinctio
+            recusandae totam quidem repudiandae omnis veritatis nostrum
+            laboriosam architecto optio rem, dignissimos voluptatum beatae
+            aperiam voluptatem atque. Beatae rerum dolores sunt.
+          </p>
+        </div>
+        <div
+          className={toggleState === 5 ? "content  active-content" : "content"}
+        >
+          <h2>Content 5</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
+            nostrum rerum laudantium totam unde adipisci incidunt modi alias!
+            Accusamus in quia odit aspernatur provident et ad vel distinctio
+            recusandae totam quidem repudiandae omnis veritatis nostrum
+            laboriosam architecto optio rem, dignissimos voluptatum beatae
+            aperiam voluptatem atque. Beatae rerum dolores sunt.
+          </p>
+        </div>
+      </div>
+
+
+
+
     </div>
-  </nav> 
-  
-</div> 
-  
-    
-   
+
+
+
+    /* <div className="PostLink">
+     <nav class="navbar navbar-expand-lg pl-md-5 ">
+        <div class="container" id="link">
+          <ul class="navbar-nav">
+            <li class="nav-item ">
+              <a class="nav-link" href="#">ALL POSTS(32)</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="##">ARTICLE</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="##">EVENT</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link "  href="##">EDUCATION</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="##">JOB</a>
+            </li>
+          </ul>
+        </div>
+      </nav> 
+      
+    </div>  */
+
+
+
   )
 }
 
