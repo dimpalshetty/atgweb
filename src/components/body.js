2c75d206269a128cb6218eb65fc48c4957f37cc8
dimpalshetty <dimpalshetty2000@gmail.com>
2021-06-16 23:08:39 +0530

    cards

--- src/components/body.js
+++ src/components/body.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import whole from '../images/whole.png';
+import Card1 from '../images/card1.png';
+import Profile from '../images/profile.png';
 import '../components/body.css';
 import SearchIcon from "@material-ui/icons/Search";
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
@@ -9,6 +10,9 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import useScript from '../useScript';
 import { Tab } from 'react-bootstrap';
 import Tabs from 'react-bootstrap/Tabs';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 
 
@@ -31,15 +35,25 @@ function Body() {
                 onSelect={(k) => setKey(k)}
               >
                 <Tab eventKey="home" title="All Posts(32)">
-                <div class="card" >
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
+                  <br />
+                  <Card style={{ width: '40rem' }}>
+                    <Card.Img variant="top" src={Card1} />
+                    <Card.Body>
+                      <Card.Title style={{ fontSize: 10 }}>✍️ Article</Card.Title>
+                      <div class="row row-xs">
+                        <div>
+                          <Card.Title style={{ paddingTop: 5, width: 500, fontWeight: 700, }} >What if famous brands had regular fonts? Meet RegulaBrands!</Card.Title> </div>
+                        <div className="more"><MoreHorizIcon /></div>
+                      </div>
+                      <Card.Text style={{ fontSize: 15, color: "#5C5C5C" }}>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum...
+                      </Card.Text>
+                      <div class="container">
+                      <div class="row row-2>
+                      <img src={Profile} className="profile" />
+                      <h2>he</h2> </div> </div>
+                    </Card.Body>
+                  </Card>
                 </Tab>
                 <Tab eventKey="profile" title="Events">
                   Hi
@@ -61,17 +75,17 @@ function Body() {
         <div class="col-sm">
 
           <div class="row">
-          <div class="btn-group">
-  <button class="btn btn-light btn-block dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-   Write A Post 
-  </button>
-  <ul class="dropdown-menu">
-    ...
-  </ul>
-</div>
-            
+            <div class="btn-group">
+              <button class="btn btn-light btn-block dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Write A Post
+              </button>
+              <ul class="dropdown-menu">
+                ...
+              </ul>
+            </div>
+
             <div className="singlebutton"><button type="button" data-toggle="collapse" class="btn btn-primary "> <GroupAddIcon /> Join A Group </button> </div>
-            
+
           </div>
         </div>
       </div>
