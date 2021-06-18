51075ea66c5a9850602a2c7bf20c3f3383a6909d
dimpalshetty <dimpalshetty2000@gmail.com>
2021-06-18 13:09:52 +0530

    errors in location

--- src/components/body.js
+++ src/components/body.js
@@ -22,7 +22,15 @@ import Col from 'react-bootstrap/Col';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import ShareIcon from '@material-ui/icons/Share';
 import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton'
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Form from 'react-bootstrap/Form';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Grid from '@material-ui/core/Grid';
+import ErrorOutlineOutlinedIcon from '@material-ui/icons/ErrorOutlineOutlined';
+import CreateIcon from '@material-ui/icons/Create';
 
 
 function Body() {
@@ -30,7 +38,7 @@ function Body() {
 
   return (
 
-    <Container style={{ paddingLeft: 40 }}  >
+    <Container style={{ paddingLeft: 40, paddingTop: 20 }}  >
       <Row>
         <Col>
           <Tabs
@@ -51,41 +59,43 @@ function Body() {
                       <Col lg={11}>
                         <Card.Title style={{ paddingTop: 5, width: 500, fontWeight: 700, }} >What if famous brands had regular fonts? Meet RegulaBrands!</Card.Title>
                       </Col>
-                      <Col lg={1} > 
-                      <div class="dropdown">
+                      <Col lg={1} >
+                        <Dropdown>
+                          <Dropdown.Toggle className="buttons" variant="none" id="dropdown-basic">
+                            <MoreHorizIcon />
+                          </Dropdown.Toggle>
 
-  
- 
-  <a  type="button" id="dropdownMenu2" data-toggle="dropdown"
-  aria-haspopup="true" aria-expanded="false">hey</a>
-    
-
-  <div class="dropdown-menu dropdown-primary">
-    <a class="dropdown-item" href="#">hey&nbsp;&nbsp;Pay</a>
-    <a class="dropdown-item" href="#">hey&nbsp;&nbsp;Disable alertss</a>
-    <a class="dropdown-item" href="#">hey&nbsp;&nbsp;Check mail</a>
-  </div>
-</div>
+                          <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                          </Dropdown.Menu>
+                        </Dropdown>
                       </Col>
                     </Row>
                   </Container>
                   <Card.Text style={{ fontSize: 15, color: "#5C5C5C", paddingLeft: 15, overflow: 'hidden' }}>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum...
                   </Card.Text>
-                  <Row style={{justifyContent:'space-between', paddingTop: 5}}>
+                  <Row style={{ justifyContent: 'space-between', paddingTop: 5 }}>
                     <Col lg={1}>    <img src={Profile} />
                     </Col>
-                    <Col lg={3} style={{ alignItems: 'center', paddingTop:8, fontWeight: 700, fontSize: 15 }}><p>Sarthak Kamra</p></Col>
-                    <Col md={{ span:1, offset: 1 }}> </Col>
-                    <Col lg={4} style={{paddingTop: 5}}><Row lg={2}><VisibilityIcon style={{ color: 'grey' , marginRight: -30, paddingTop: 5, }} />
+                    <Col lg={7} style={{ alignItems: 'center', paddingTop: 8, fontWeight: 700, fontSize: 15 }}><p>Sarthak Kamra</p></Col>
+                    <Col md={{ span: 1, offset: 1 }}> </Col>
+                    <Col lg={3} style={{ paddingTop: 5 }}><Row lg={1}><VisibilityIcon style={{ color: 'grey', marginRight: -30, paddingTop: 5, }} />
                       <p className="Views">1.4k Views</p></Row>
                     </Col>
-                    <Col lg={2}><Button style={{ backgroundColor: '#EDEEF0', fontSize: 10}} variant="contained"><ShareIcon style={{ color: 'black' }} /></Button></Col>
+                    <Col lg={1}>
+
+
+                      <Button size="sm" style={{ backgroundColor: '#EDEEF0', width: 40 }} variant="contained"><ShareIcon style={{ color: 'black' }} />
+
+                      </Button></Col>
                   </Row>
                 </Card.Body>
               </Card>
 
-              <br/>
+              <br />
               <Card className="postCard" style={{ width: '40rem', objectFit: 'cover' }}>
                 <Card.Img variant="top" src={Card2} />
                 <Card.Body>
@@ -93,87 +103,131 @@ function Body() {
                   <Container>
                     <Row>
                       <Col>
-                        <Card.Title style={{ paddingTop: 5, width: 500, fontWeight: 700, }} >Tax Benefits for Investment under National Pension Scheme launched by Government</Card.Title>
+                        <Card.Title style={{ paddingTop: 5, width: 480, fontWeight: 700, }} >Tax Benefits for Investment under National Pension Scheme launched by Government</Card.Title>
                       </Col>
-                      <Col> <MoreHorizIcon /> </Col>
+                      <Col lg={1} ></Col>
+                      <Dropdown>
+                        <Dropdown.Toggle className="buttons" variant="none" id="dropdown-basic">
+                          <MoreHorizIcon />
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        </Dropdown.Menu>
+                      </Dropdown>
+                      <Col />
                     </Row>
                   </Container>
                   <Card.Text style={{ fontSize: 15, color: "#5C5C5C", paddingLeft: 15, overflow: 'hidden' }}>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum...
                   </Card.Text>
-                  <Row style={{justifyContent:'space-between', paddingTop: 5}}>
+                  <Row style={{ justifyContent: 'space-between', paddingTop: 5 }}>
                     <Col lg={1}>    <img src={Profile2} />
                     </Col>
-                    <Col lg={7} style={{ alignItems: 'center', paddingTop:8, fontWeight: 700, fontSize: 15 }}><p>Sarah West</p></Col>
-                    <Col lg={3} style={{paddingTop: 5}}><Row lg={1}><VisibilityIcon style={{ color: 'grey' , marginRight: -30, paddingTop: 5, }} />
+                    <Col lg={7} style={{ alignItems: 'center', paddingTop: 8, fontWeight: 700, fontSize: 15 }}><p>Sarah West</p></Col>
+                    <Col lg={3} style={{ paddingTop: 5 }}><Row lg={1}><VisibilityIcon style={{ color: 'grey', marginRight: -30, paddingTop: 5, }} />
                       <p className="Views">1.4k Views</p></Row>
                     </Col>
                     <Col lg={1}>
 
-                    
-                    <Button size="sm" style={{ backgroundColor: '#EDEEF0', width: 40}} variant="contained"><ShareIcon style={{ color: 'black' }} />
-                    
-                    </Button></Col>
+
+                      <Button size="sm" style={{ backgroundColor: '#EDEEF0', width: 40 }} variant="contained"><ShareIcon style={{ color: 'black' }} />
+
+                      </Button></Col>
                   </Row>
                 </Card.Body>
               </Card>
-              <br/>
+              <br />
               <Card className="postCard" style={{ width: '40rem', objectFit: 'cover' }}>
-                <Card.Img variant="top" src={Card1} />
+                <Card.Img variant="top" src={Card2} />
                 <Card.Body>
-                  <Card.Title style={{ fontSize: 10, paddingLeft: 15, }}>✍️ Article </Card.Title>
+                  <Card.Title style={{ fontSize: 10, paddingLeft: 15, }}>🔬 Education </Card.Title>
                   <Container>
                     <Row>
                       <Col>
-                        <Card.Title style={{ paddingTop: 5, width: 500, fontWeight: 700, }} >Finance & Investment Elite Social Mixer @Lujiazui</Card.Title>
+                        <Card.Title style={{ paddingTop: 5, width: 480, fontWeight: 700, }} >Tax Benefits for Investment under National Pension Scheme launched by Government</Card.Title>
                       </Col>
-                      <Col> <MoreHorizIcon /> </Col>
+                      <Col lg={1} ></Col>
+                      <Dropdown>
+                        <Dropdown.Toggle className="buttons" variant="none" id="dropdown-basic">
+                          <MoreHorizIcon />
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        </Dropdown.Menu>
+                      </Dropdown>
+                      <Col />
                     </Row>
                   </Container>
                   <Card.Text style={{ fontSize: 15, color: "#5C5C5C", paddingLeft: 15, overflow: 'hidden' }}>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum...
                   </Card.Text>
-                  <Row style={{justifyContent:'space-between', paddingTop: 5}}>
-                    <Col lg={1}>    <img src={Profile} />
+                  <Row style={{ justifyContent: 'space-between', paddingTop: 5 }}>
+                    <Col lg={1}>    <img src={Profile2} />
                     </Col>
-                    <Col lg={3} style={{ alignItems: 'center', paddingTop:8, fontWeight: 700, fontSize: 15 }}><p>Sarthak Kamra</p></Col>
-                    <Col md={{ span:1, offset: 1 }}> </Col>
-                    <Col lg={4} style={{paddingTop: 5}}><Row lg={2}><VisibilityIcon style={{ color: 'grey' , marginRight: -30, paddingTop: 5, }} />
+                    <Col lg={7} style={{ alignItems: 'center', paddingTop: 8, fontWeight: 700, fontSize: 15 }}><p>Sarah West</p></Col>
+                    <Col lg={3} style={{ paddingTop: 5 }}><Row lg={1}><VisibilityIcon style={{ color: 'grey', marginRight: -30, paddingTop: 5, }} />
                       <p className="Views">1.4k Views</p></Row>
                     </Col>
-                    <Col lg={2}><Button style={{ backgroundColor: '#EDEEF0', fontSize: 10}} variant="contained"><ShareIcon style={{ color: 'black' }} /></Button></Col>
+                    <Col lg={1}>
+
+
+                      <Button size="sm" style={{ backgroundColor: '#EDEEF0', width: 40 }} variant="contained"><ShareIcon style={{ color: 'black' }} />
+
+                      </Button></Col>
                   </Row>
                 </Card.Body>
               </Card>
-              <br/>
+              <br />
               <Card className="postCard" style={{ width: '40rem', objectFit: 'cover' }}>
-                <Card.Img variant="top" src={Card1} />
+                <Card.Img variant="top" src={Card2} />
                 <Card.Body>
-                  <Card.Title style={{ fontSize: 10, paddingLeft: 15, }}>✍️ Article </Card.Title>
+                  <Card.Title style={{ fontSize: 10, paddingLeft: 15, }}>🔬 Education </Card.Title>
                   <Container>
                     <Row>
                       <Col>
-                        <Card.Title style={{ paddingTop: 5, width: 500, fontWeight: 700, }} >What if famous brands had regular fonts? Meet RegulaBrands!</Card.Title>
+                        <Card.Title style={{ paddingTop: 5, width: 480, fontWeight: 700, }} >Tax Benefits for Investment under National Pension Scheme launched by Government</Card.Title>
                       </Col>
-                      <Col> <MoreHorizIcon /> </Col>
+                      <Col lg={1} ></Col>
+                      <Dropdown>
+                        <Dropdown.Toggle className="buttons" variant="none" id="dropdown-basic">
+                          <MoreHorizIcon />
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        </Dropdown.Menu>
+                      </Dropdown>
+                      <Col />
                     </Row>
                   </Container>
                   <Card.Text style={{ fontSize: 15, color: "#5C5C5C", paddingLeft: 15, overflow: 'hidden' }}>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum...
                   </Card.Text>
-                  <Row style={{justifyContent:'space-between', paddingTop: 5}}>
-                    <Col lg={1}>    <img src={Profile} />
+                  <Row style={{ justifyContent: 'space-between', paddingTop: 5 }}>
+                    <Col lg={1}>    <img src={Profile2} />
                     </Col>
-                    <Col lg={3} style={{ alignItems: 'center', paddingTop:8, fontWeight: 700, fontSize: 15 }}><p>Sarthak Kamra</p></Col>
-                    <Col md={{ span:1, offset: 1 }}> </Col>
-                    <Col lg={4} style={{paddingTop: 5, right: 20,}}><Row lg={2} style={{paddingRight: 10}}><VisibilityIcon style={{ color: 'grey' , marginRight: -30, paddingTop: 5, }} />
+                    <Col lg={7} style={{ alignItems: 'center', paddingTop: 8, fontWeight: 700, fontSize: 15 }}><p>Sarah West</p></Col>
+                    <Col lg={3} style={{ paddingTop: 5 }}><Row lg={1}><VisibilityIcon style={{ color: 'grey', marginRight: -30, paddingTop: 5, }} />
                       <p className="Views">1.4k Views</p></Row>
                     </Col>
-                    <Col lg={2}><Button style={{ backgroundColor: '#EDEEF0', fontSize: 10}} variant="contained"><ShareIcon style={{ color: 'black' }} /></Button></Col>
+                    <Col lg={1}>
+
+
+                      <Button size="sm" style={{ backgroundColor: '#EDEEF0', width: 40 }} variant="contained"><ShareIcon style={{ color: 'black' }} />
+
+                      </Button></Col>
                   </Row>
                 </Card.Body>
               </Card>
-              <br/>
+              <br />
             </Tab>
 
 
@@ -192,12 +246,54 @@ function Body() {
             </Tab>
 
 
-
           </Tabs>
 
         </Col>
 
-        <Col className="coldissapear"> <h2>hi</h2></Col>
+        <Col className="coldissapear">
+          <Row>
+
+            <Dropdown>
+              <Dropdown.Toggle style={{ fontWeight: 500, fontSize: 15 }} variant="light" id="dropdown-basic">
+                Write A Post <ArrowDropDownIcon />
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
+
+            <Col><button class="btn btn-primary" type="submit"><GroupAddIcon />  Join Group</button> </Col>
+
+          </Row>
+          
+        
+        
+         <Row>
+         <Col styles={{paddingTop:100}}>
+         <Container styles={{paddingTop:50000}}>
+        
+          <LocationOnIcon />
+          <TextField
+          className="textfield"
+          fullWidth={false}
+          id="standard-helperText"
+          defaultValue="Noida, India"
+          
+        />
+         <CreateIcon/>
+
+         </Container>
+         </Col>
+         </Row>
+        
+      
+
+        </Col>
+
 
       </Row>
     </Container>
